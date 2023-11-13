daa4a4d594c93206c89b495d9409af323259fa8e
Stacey robson <stacey.robson.sr@gmail.com>
2023-11-13 18:08:20 +0000

    django first run through

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-57ubvph&n0x!7#i$3b1)jwylg2gk*3uy_h(*57(s!*=7^75e0e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-staceyrb89-djangoprojec-2zrnd00sl0d.ws-eu106.gitpod.io']
+ALLOWED_HOSTS = ['8000-staceyrb89-djangoprojec-3line715u52.ws-eu106.gitpod.io','.herokuapp.com']
 
 
 
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'hello_world',
+    'about',
 ]
 
 MIDDLEWARE = [
